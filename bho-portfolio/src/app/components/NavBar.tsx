b12018c529a316afe97571fb3dc6f030b97bea54
iphonecats <bho@gatech.edu>
2023-12-23 19:50:04 -0500

    Darker nav bar

--- bho-portfolio/src/app/components/NavBar.tsx
+++ bho-portfolio/src/app/components/NavBar.tsx
@@ -35,7 +35,7 @@ const NavBar = () => {
     const pathname = usePathname()
 
     return (
-        <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-blue-200">
+        <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-blue-300">
             <div className="justify-between md:item-center md:flex">
                 <div>
                     <div className="flex items-center justify-between py-3 md:py-5 md:block">
