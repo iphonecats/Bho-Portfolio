b48c85fb43757db45d35cf49303324c331bb50fe
iphonecats <bho@gatech.edu>
2023-12-23 19:34:03 -0500

    head fix attempt

--- bho-portfolio/src/app/layout.tsx
+++ bho-portfolio/src/app/layout.tsx
@@ -3,20 +3,15 @@ import './globals.css'
 import NavBar from './components/NavBar'
 import { ThemeProvider } from 'next-themes'
 import Footer from './components/Footer'
-import Head from 'next/head'
 
 export default function RootLayout({
   children,
-  pageTitle = 'Brandon Ho',
 }: {
   children: React.ReactNode
-  pageTitle?: string;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-        <title>{pageTitle}</title>
-      </Head>
+      <head />
       <body>
         <ThemeProvider enableSystem={true} attribute="class">
           <NavBar />
