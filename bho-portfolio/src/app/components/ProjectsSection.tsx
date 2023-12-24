8011b94f5c8a37c1a2acbf4ebfb707fb046620d1
iphonecats <bho@gatech.edu>
2023-12-23 21:24:13 -0500

    fixed footer errors

--- bho-portfolio/src/app/components/ProjectsSection.tsx
+++ bho-portfolio/src/app/components/ProjectsSection.tsx
@@ -16,7 +16,7 @@ const projects = [
   {
     name: "This Website!",
     description:
-      "A personal website created using JavaScript, Typescript, and React with Next.js styled with Tailwind CSS",
+      "A personal website created using Typescript and React with Next.js styled with Tailwind CSS",
     image: "/images/Portfolio.png",
     github: "https://github.com/iphonecats/Bho-Portfolio",
     link: "https://www.linkedin.com/in/brandondho",
