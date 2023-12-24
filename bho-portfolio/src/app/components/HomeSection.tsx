ccdf5bb1b526c8b6af17669edaefd71d9ce2ea2a
iphonecats <bho@gatech.edu>
2023-12-23 20:47:06 -0500

    fix index

--- bho-portfolio/src/app/components/HomeSection.tsx
+++ bho-portfolio/src/app/components/HomeSection.tsx
@@ -7,40 +7,40 @@ import { HiArrowDown } from "react-icons/hi"
 const HomeSection = () => {
   return (
     <section id="home">
-        <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-10 md:py-20 md:flex-row md:space-x-4 md:text-left">
-            <div>
-                <Image
-                    src={"/images/headshot.png"}
-                    alt=""
-                    width={325}
-                    height={325}
-                    className="rounded-full shadow-2xl"
-                />
-            </div>
-            <div className="md:mt-2 md:w-3/5">
-                <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Brandon!</h1>
-                <p className="text-lg mt-4 mb-6 md:text-2xl">
-                    I&#39;m a{" "}
-                    <span className="font-semibold text-blue-400">
-                    Software Engineer{" "}
-                    </span>
-                    based in Atlanta, GA. Working on meaningful projects
-                    to make life easier for people!
-                </p>
-            </div>
-            <div className="flex flex-row items-center text-center justify-center ">
-                <Link
-                    to="about"
-                    activeClass="active"
-                    spy={true}
-                    smooth={true}
-                    offset={-100}
-                    duration={500}
-                >
-                    <HiArrowDown size={35} className="animate-bounce" />
-                </Link>
-            </div>
+      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-10 md:py-20 md:flex-row md:space-x-4 md:text-left">
+        <div>
+          <Image
+            src={"/images/headshot.png"}
+            alt=""
+            width={325}
+            height={325}
+            className="rounded-full shadow-2xl"
+          />
         </div>
+        <div className="md:mt-2 md:w-3/5">
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Brandon!</h1>
+          <p className="text-lg mt-4 mb-6 md:text-2xl">
+            I&#39;m a{" "}
+            <span className="font-semibold text-blue-400">
+            Software Engineer{" "}
+            </span>
+            based in Atlanta, GA. Working on meaningful projects
+            to make life easier for people!
+          </p>
+        </div>
+        <div className="flex flex-row items-center text-center justify-center ">
+          <Link
+            to="about"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            <HiArrowDown size={35} className="animate-bounce" />
+          </Link>
+        </div>
+      </div>
     </section>
   )
 }
--- bho-portfolio/src/app/components/NavBar.tsx
+++ bho-portfolio/src/app/components/NavBar.tsx
@@ -2,91 +2,85 @@
 import React from 'react'
 import { useState } from "react"
 import { Link } from "react-scroll/modules"
-import { usePathname } from "next/navigation"
-import { useTheme } from "next-themes"
-import { RiMoonFill, RiSunLine } from "react-icons/ri"
 import { IoMdMenu, IoMdClose } from "react-icons/io"
 
 interface NavItem {
-    label: string
-    page: string
+  label: string
+  page: string
 }
 
 const NAV_ITEMS: Array<NavItem> = [
-    {
-        label: "Home",
-        page: "home",
-    },
-    {
-        label: "About",
-        page: "about",
-    },
-    {
-        label: "Projects",
-        page: "projects",
-    },
+  {
+    label: "Home",
+    page: "home",
+  },
+  {
+    label: "About",
+    page: "about",
+  },
+  {
+    label: "Projects",
+    page: "projects",
+  },
 ]
 
 const NavBar = () => {
 
-    const { systemTheme, theme, setTheme } = useTheme()
-    const currentTheme = theme === "system" ? systemTheme : theme
-    const [navbar, setNavbar] = useState(false)
-    const pathname = usePathname()
+  const [navbar, setNavbar] = useState(false)
 
-    return (
-        <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-blue-300">
-            <div className="justify-between md:item-center md:flex">
-                <div>
-                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                        <Link to="home">
-                            <div className="container flex items-center space-x-2">
-                                <h2 className="text-2xl font-bold">Brandon Ho</h2>
-                            </div>
-                        </Link>
-                        <div className="md:hidden">
-                            <button
-                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                                onClick={() => setNavbar(!navbar)}
-                            >
-                                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                    <div
-                        className={`items-center flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                            navbar ? "block" : "hidden"
-                        }`}
-                    >
-                        <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                            {NAV_ITEMS.map((item, idx) => {
-                                return (
-                                <Link
-                                    key={idx}
-                                    to={item.page}
-                                    className={
-                                        "block lg:inline-block hover:text-neutral-500"
-                                    }
-                                    activeClass="active"
-                                    spy={true}
-                                    smooth={true}
-                                    offset={-100}
-                                    duration={500}
-                                    onClick={() => setNavbar(!navbar)}
-                                >
-                                    {item.label}
-                                </Link>
-                                )
-                            })}
-                        </div>
-                    </div>
-                </div>
+  return (
+    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-blue-300">
+      <div className="justify-between md:item-center md:flex">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <Link to="home">
+              <div className="container flex items-center space-x-2">
+                <h2 className="text-2xl font-bold">Brandon Ho</h2>
+              </div>
+            </Link>
+            <div className="md:hidden">
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
+                >
+                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+              </button>
             </div>
-        </header>
-    )
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between py-3 md:py-5 md:block">
+          <div
+            className={`items-center flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              {NAV_ITEMS.map((item, idx) => {
+                return (
+                  <Link
+                    key={idx}
+                    to={item.page}
+                    className={
+                        "block lg:inline-block hover:text-neutral-500"
+                    }
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    onClick={() => setNavbar(!navbar)}
+                  >
+                    {item.label}
+                  </Link>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default NavBar
